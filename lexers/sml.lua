a492db0c65df14993906a7014d3354a2e32472b4
Hackerpilot <briancschott@gmail.com>
2016-08-09 21:20:23 -0700

    Fix capitalization

--- lexers/sml.lua
+++ lexers/sml.lua
@@ -4,7 +4,7 @@ local l = require('lexer')
 local token, word_match = l.token, l.word_match
 local P, R, S = lpeg.P, lpeg.R, lpeg.S
 
-local M = {_NAME = 'SML'}
+local M = {_NAME = 'sml'}
 
 -- Whitespace.
 local ws = token(l.WHITESPACE, l.space^1)
